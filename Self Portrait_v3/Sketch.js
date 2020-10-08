23ef60062b78268b563b910ad0c9079cb1c30618
christiannoa <christiannoa20@gmail.com>
2020-10-08 17:54:26 -0400

    add interactive self portrait

---
+++ Self Portrait_v3/Sketch.js
@@ -0,0 +1,54 @@
+/*
+
+	Self portrait v2
+	by Christian Noa
+
+*/
+
+//global scope
+var x = 350; //character origin
+var y = 225; //character origin
+function setup() {
+	createCanvas(720, 420);
+
+}
+
+function draw() {
+	background('Grey');
+
+	// face
+	noStroke();
+	fill("white");
+	square(x - 125, y - 145, 250);
+
+	//eyebrows
+	stroke("#000000"); //black
+	strokeWeight(5);
+	line(x - 75, y - 125, x - 40, y - 100); //left
+
+	// eyes
+	stroke("#000000");	//black
+	strokeWeight(7);
+	circle(x - 50, y - 50, 50); //left
+	circle(x + 50, y - 50, 50); //right
+	circle(x - 50, y - 50, 75); //left
+	circle(x + 50, y - 50, 75); //right
+
+	fill("#000000"); //black
+	circle(x - 50, y - 50, 50); //left
+	circle(x + 50, y - 50, 50); //right
+	
+	fill("white"); //white
+	circle(x - mouseX, y - mouseY, 25); //left
+	circle(x + mouseX, y - mouseY, 25); //right
+
+	//mouth
+	strokeWeight(5);
+	line(x + 50, y + 35, x-50, y + 25);
+
+
+	// stroke('green');
+	// circle(x, y, 5)
+
+
+}
