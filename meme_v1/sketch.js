7a31a664a78c0586a9a7a9a455ddb3da059f112a
christiannoa <christiannoa20@gmail.com>
2020-10-15 16:27:20 -0400

    meme - add image

--- meme_v1/sketch.js
+++ meme_v1/sketch.js
@@ -3,9 +3,14 @@ meme
 
 */
 
-var title = 'COTF';
-var description = 'comrade of the future amet, consectetur adipiscing elit. Nunc dignissim scelerisque dui,at scelerisque turpis egestas ac. Vestibulum malesuada nisl tellus, aliquet mollis lectus rutrum non. Pellentesque ornare, lorem ac pretium tempor, nulla purus auctor quam, sit amet porttitor ligula quam ut mi. Nulla sit amet fermentum leo. Duis ornare hendrerit justo. Aenean ultrices ligula nec pharetra tincidunt. Fusce pellentesque diam id fermentum posuere. Maecenas eget velit vitae lorem congue iaculis eget dignissim sem. Donec eleifend convallis ipsum, mattis accumsan lacus blandit at. Cras vitae justo sit amet dui pretium aliquet rhoncus a purus. Maecenas lacus erat, interdum ut luctus sed, maximus in dolor. Nulla nec rutrum arcu.';
+var title = 'I did not get fired!';
+var description = 'Im a liar, a thief, and a fat looser. As far as the bridge, YES I knew about it. Im a liar, a thief, and a fat looser. As far as the bridge, I knew about it. Im a liar, a thief, and a fat looser. As far as the bridge, I knew about it. Im a liar, a thief, and a fat looser. As far as the bridge, I knew about it. Im a liar, a thief, and a fat looser. As far as the bridge, I knew about it.';
 
+
+function preload(){
+	govchristie = loadImage('gov_christie.jpg');
+
+}
 
 function setup () {
 	createCanvas (640,360);
@@ -15,9 +20,16 @@ function setup () {
 function draw() {
 	background(220);
 
-	textSize(75);
+
+
+	/* image */
+	image(govchristie, 0, 0, width, height);
+
+
+
+	textSize(70);
 	fill('crimson');
-	stroke('black');
+	stroke('white');
 	strokeWeight(4);
 	textFont('Merriweather');
 	textStyle(ITALIC);
@@ -32,12 +44,13 @@ function draw() {
 	var titleWidth = textWidth (title);
 
 
-	textSize(16);
-	fill('black');
-	noStroke();
+	textSize(18);
+	fill('white');
+	stroke('crimson');
+	strokeWeight(1);
 	textFont('Helvetica');
 	textLeading(mouseY);
-	text(description, width / 2 - titleWidth / 2, height / 2 + 10, titleWidth);
+	text(description, width / 2 - titleWidth / 2, height / 2 + 30, titleWidth);
 
 
 }
